f25ba18a414a5c13e332599c1756f789b70491e9
Buffden <buffdenslays@gmail.com>
2025-03-10 12:52:42 -0500

    update changes related to amazon sdk for RDS and implemented IAM roles in the application Properties and test

--- src/test/java/com/ems/employee_management_system/EmployeeManagementSystemApplicationTests.java
+++ src/test/java/com/ems/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -4,29 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class EmployeeManagementSystemApplicationTests {
-
-	static {
-		String dbUser = System.getenv("DB_CREDENTIALS_USR");
-		String dbPassword = System.getenv("DB_CREDENTIALS_PSW");
-
-		if (dbUser == null || dbPassword == null) {
-			Dotenv dotenv = Dotenv.configure()
-					.directory("./")
-					.load();
-			dbUser = dotenv.get("DB_CREDENTIALS_USR");
-			dbPassword = dotenv.get("DB_CREDENTIALS_PSW");
-		}
-
-		System.setProperty("DB_CREDENTIALS_USR", dbUser);
-		System.setProperty("DB_CREDENTIALS_PSW", dbPassword);
-	}
-
 	@Test
 	void contextLoads() {
-		// Your test logic here
+		// Just validate Spring Boot context loads successfully
+		assertTrue(true);
 	}
 }
